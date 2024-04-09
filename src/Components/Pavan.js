81e655afa9f3946c8af5b4eff206acfe751d0814
Pavan Karthik Udayagiri <pavankarthik634@gmail.com>
2024-04-09 17:00:42 +0530

    Python

--- src/Components/Pavan.js
+++ src/Components/Pavan.js
@@ -16,7 +16,7 @@ function Pavan() {
 
       <div className="container">
         <pre></pre>
-        <p className="text">I am a self taught full stack developer based in Bangalore. I hold a B.Tech degree in Electrical and Electronics Engineering. I started my professional journey as a SAP Hybris Developer at Pearson, where I have been working on issues related to E-commerce websites. Exploring these websites sparked my interest, and I decided to learn more about web development. The more I learned about web development, the more I fell in love with it!</p>
+        <p className="text">I am a self taught full stack python developer based in Bangalore. I hold a B.Tech degree in Electrical and Electronics Engineering. I started my professional journey as a SAP Hybris Developer at Pearson, where I have been working on issues related to E-commerce websites. Exploring these websites sparked my interest, and I decided to learn more about web development. The more I learned about web development, the more I fell in love with it!</p>
         <pre></pre>
         <p className="text">Outside of coding, I also enjoy traveling, cooking, gaming, watching  shows and movies, reading, trekking and spending time with family.</p>
       </div>
